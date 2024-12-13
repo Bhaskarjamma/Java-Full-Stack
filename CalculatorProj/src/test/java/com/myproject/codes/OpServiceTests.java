b8c38139e72bea9ff072f1e3308199f2f9484197
Bhaskarjamma <156530575+Bhaskarjamma@users.noreply.github.com>
2024-12-13 15:51:28 +0530

    Update OpServiceTests.java

--- CalculatorProj/src/test/java/com/myproject/codes/OpServiceTests.java
+++ CalculatorProj/src/test/java/com/myproject/codes/OpServiceTests.java
@@ -1,32 +1,32 @@
-//package com.myproject.codes;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//public class OpServiceTests {
-//
-//    @Autowired
-//    private OpService opService;
-//
-//    @Test
-//    public void testAddition() {
-//        assertEquals(5, opService.artheOp('+', 2, 3));
-//    }
-//
-//    @Test
-//    public void testDivisionByZero() {
-//        Exception exception = assertThrows(UnsupportedOperationException.class, () -> {
-//            opService.artheOp('/', 5, 0);
-//        });
-//
-//        assertTrue(exception.getMessage().contains("cannot divide by zero"));
-//    }
-//
-//    @Test
-//    public void testEvaluation() {
-//        assertEquals(7, opService.EvalInfixEx("3 + 4"));
-//    }
-//}
+package com.myproject.codes;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OpServiceTests {
+
+   @Autowired
+   private OpService opService;
+
+   @Test
+   public void testAddition() {
+       assertEquals(5, opService.artheOp('+', 2, 3));
+   }
+
+   @Test
+   public void testDivisionByZero() {
+       Exception exception = assertThrows(UnsupportedOperationException.class, () -> {
+           opService.artheOp('/', 5, 0);
+       });
+
+       assertTrue(exception.getMessage().contains("cannot divide by zero"));
+   }
+
+   @Test
+   public void testEvaluation() {
+       assertEquals(7, opService.EvalInfixEx("3 + 4"));
+   }
+}
